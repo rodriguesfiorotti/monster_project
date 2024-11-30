d62dbd2a64be3653443751709447dac213f97500
João Victor Rodrigues <rodriguesfiorotti@gmail.com>
2024-11-29 22:01:51 -0300

    Alguns Ajustes

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 function toKebabCase(str) {                           
-    return str.split(" ").join("-").toLowerCase()     
+    return str.split(" ").join("-").toLowerCase();    
+}
+
+function imgMonsterConfig(element, src, alt) {
+    const monsterInfo = document.createElement("div");
+    monsterInfo.classList.add("monster-info");  // Adicionando a classe para controlar a imagem
+    const MonsterImg = document.createElement("img");
+    MonsterImg.src = src;
+    MonsterImg.alt = alt;
+    monsterInfo.appendChild(MonsterImg);
+    document.getElementById('monsterList').appendChild(monsterInfo);
 }
 
 function adicionarLi(monster) {                       
-    const liElement = document.createElement("li")
-    liElement.id = toKebabCase(monster.nome)
-    liElement.textContent = `Nome: ${monster.nome} | Elemento: ${monster.element} | Pontos de Vida: ${monster.health} | Habilidades: ${monster.habilities}` 
+    const liElement = document.createElement("li");
+    liElement.id = toKebabCase(monster.nome);
+    liElement.textContent = `Nome: ${monster.nome} | Elemento: ${monster.element} | Pontos de Vida: ${monster.health} | Habilidades: ${monster.habilities}`; 
 
-    document.getElementById("monsterList").appendChild(liElement)
+    // Adicionando o monstro na lista
+    document.getElementById("monsterList").appendChild(liElement);
 
-    if (monster.element.value === 'Fogo') {
-        fireMonsterImg = document.createElement("img") 
-        fireMonsterImg.src = "Images/fire_monster.webp"
-        fireMonsterImg.alt = "Monstro de Fogo"
-
-        document.getElementById("monsterList").appendChild(liElement)
-        document.getElementById(liElement.id).appendChild(fireMonsterImg)
+    switch (monster.element) {
+        case 'Fogo':
+            imgMonsterConfig(monster.element, "Images/fire_monster.webp", "Monstro de Fogo");
+            break;
+        case 'Água':
+            imgMonsterConfig(monster.element, "Images/water_monster.webp", "Monstro de Água");
+            break;
+        case 'Terra':
+            imgMonsterConfig(monster.element, "Images/earth_monster.webp", "Monstro de Terra");
+            break;
+        case 'Ar':
+            imgMonsterConfig(monster.element, "Images/air_monster.webp", "Monstro de Ar");
+            break;
+        default:
+            break;
     }
 }
 
@@ -23,13 +42,13 @@ const form = document.getElementById("registerMonster");
 
 form.addEventListener("submit", function (evento) {  
     evento.preventDefault();       
-    const monster = {}
+    const monster = {};
     monster.nome = document.getElementById("monsterName").value;
     monster.element = document.getElementById("monsterElement").value;
     monster.health = document.getElementById("monsterHealth").value; 
     monster.habilities = document.getElementById("monsterHabilities").value;  
 
-    adicionarLi(monster)     
+    adicionarLi(monster);
 
-    evento.target.reset()  
-})
+    evento.target.reset();  
+});
